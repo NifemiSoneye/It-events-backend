e1ab66e549696b0f16940bcf5b1a502c991304b8
NifemiSoneye <nifemisoneye@gmail.com>
2026-04-10 09:51:21 -0700

    Debugged corsOptions

--- src/config/corsOptions.ts
+++ src/config/corsOptions.ts
@@ -1,9 +1,11 @@
+import allowedOrigins from "./allowedOrigins";
 import { CorsOptions } from "cors";
-const allowedOrigins = require("./allowedOrigins");
+
+console.log("allowedOrigins:", allowedOrigins, Array.isArray(allowedOrigins));
 
 const corsOptions: CorsOptions = {
   origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) != -1 || !origin) {
+    if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
@@ -12,4 +14,5 @@ const corsOptions: CorsOptions = {
   credentials: true,
   optionsSuccessStatus: 200,
 };
+
 export default corsOptions;
