6bd00d879eb7afa4a788af698bb77b5d3605977c
NifemiSoneye <nifemisoneye@gmail.com>
2026-04-18 10:23:55 -0700

    Debugged allowed origins array

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -1,6 +1,6 @@
 const allowedOrigins: string[] = [
   "http://localhost:5173",
-  "https://tech-event-teal.vercel.app/",
+  "https://tech-event-teal.vercel.app",
 ];
 
 export default allowedOrigins;
